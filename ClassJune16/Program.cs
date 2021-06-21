4af67b51f6f9fcd97ae83ed5a1bc47d2a085dfe6
clarke <cf.lamb25@gmail.com>
2021-06-21 16:46:25 -0400

    First commit for Regex Lab

---
+++ ClassJune16/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ClassJune16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    try
+            //    {
+            //        var userInput = GetUserInput();
+            //        var parsedValue = int.Parse(userInput);
+            //        Console.WriteLine(parsedValue);
+            //    }
+            //    catch (Exception exception)
+            //    {
+            //        Console.WriteLine("Invalid input. Check that you entered a number.", exception.Message);
+
+            //    }
+
+
+            //Write a method that take two numbers and divides > print result to screen
+            try
+            {
+                var parsedNumber = int.Parse("Lebron James is a GOAT");
+                Console.WriteLine(divideNumber(10, 5));
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //you got the info but still want it to blow up
+                throw; 
+            }
+
+                 //Catches any other mathematical error
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //very broad way to catch any error (not very specific like the two above)
+            catch (Exception ex)
+            {             
+                                          //this will provide a reasoning for you. nope is not needed.
+                Console.WriteLine("Nope", ex.Message);
+               
+            }
+            finally
+            {
+                //meant for classes or objects that are dealing with unmanaged resource
+                //Write to a file 
+                Console.WriteLine("I have executed.");
+            }
+           
+        }
+
+        
+        static double divideNumber(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        //static string GetUserInput()
+        //{
+        //    Console.WriteLine("Enter a number");
+        //    return Console.ReadLine();
+        //}
+    }
+}
---
+++ June16validatingdata/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace June16validatingdata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateUserInput();
+            ValidateUserEmail();
+            ValidateUserName();
+            ValidateTheDate();
+        }
+
+        static void ValidateUserInput()
+        {
+            Regex pattern = new Regex(@"(\d{3}-)(\d{3}-)(\d{4}-)");
+            Console.WriteLine("Please enter valid phone number.");
+            var phoneNumber = Console.ReadLine();
+
+            if (pattern.IsMatch(phoneNumber))
+            {
+                Console.WriteLine("This is a real number");
+
+            }
+            else
+            {
+                Console.WriteLine("This number is not real.");
+            }
+        }
+
+        static void ValidateUserEmail()
+        {
+            Regex emailPattern = new Regex(@"(\w+)@(\w+).(\w{2,3})");
+            Console.WriteLine("enter a valid email");
+            var emailAddress = Console.ReadLine();
+
+            if (emailPattern.IsMatch(emailAddress))
+            {
+                Console.WriteLine("Valid email.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email.");
+            }
+        }
+
+        static void ValidateUserName()
+        {
+            Regex userName = new Regex(@"([A-Z]|[a-z])");
+            Console.WriteLine("Enter a name.");
+            var name = Console.ReadLine();
+
+            if (userName.IsMatch(name))
+            {
+                Console.WriteLine("Valid name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name.");
+            }
+
+        }
+
+        static void ValidateTheDate()
+        {
+            Regex userDate = new Regex(@"(\d{2}/)(\d{2}/)(\d{4})");
+            Console.WriteLine("Enter the date.");
+            var date = Console.ReadLine();
+
+            if (userDate.IsMatch(date))
+            {
+                Console.WriteLine("Valid date.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date.");
+            }
+
+
+        }
+    }
+}
